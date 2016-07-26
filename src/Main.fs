51965cab5d98f1ac5224f074e6d80c60a5c94692
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-07-26 16:13:23 +0200

    Try to add react-native-image-crop-picker

---
+++ src/Main.fs
@@ -0,0 +1,50 @@
+module HelloWorld
+
+open System
+open Fable.Core
+open Fable.Import
+open Fable.Import.ReactNative
+open Fable.Import.ReactImagePicker
+
+type RN = ReactNative.Globals
+type IP = ReactImagePicker.Globals
+
+type ImagePickerAppState = {
+    uri: string
+}
+
+type ImagePickerApp (props) as this =
+    inherit React.Component<obj,ImagePickerAppState>(props)
+    do this.state <- { uri = "Click me" }
+
+    member x.render () =
+
+        let buttonProps =
+             let t = createEmpty<TouchableHighlightProperties>
+             t.onPress 
+                <- (fun () -> 
+                        let p = createEmpty<ImagePickerOptions>
+                        p.title <- unbox "Meter device"
+                        p.allowsEditing <- unbox true
+
+                        IP.ImagePicker.showImagePicker(p, fun result -> 
+                            x.setState { 
+                                uri = 
+                                    if not result.didCancel then
+                                        if String.IsNullOrEmpty result.error then
+                                            result.uri
+                                        else
+                                            result.error
+                                    else
+                                        "dialog canceled" }))
+                    |> unbox
+             t
+
+
+        let highlight = React.createElement(RN.TouchableHighlight, buttonProps, [| React.createElement(RN.Text, unbox null, unbox x.state.uri) |> unbox |])      
+
+        React.createElement(RN.View, unbox null,
+            [|
+                highlight |> unbox
+            |]
+        )
